dfb7fd0793ed83f5d96d38550ba3c3027ea72a0e
Tobias Brumhard <code@brumhard.com>
2022-11-05 18:56:00 +0100

    Initialize screens

---
+++ app/lib/screens/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatelessWidget {
+  static String route = "/login";
+
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ app/lib/services/users.dart
@@ -0,0 +1,7 @@
+const baseUrl = "users.connact.fbr.ai";
+
+// GET /user => returns own data
+// GET /user/:username => returns desired user if you are in the desired users contacts field
+// POST /user => creates your own user
+// PUT /user => updates your own user
+// DELETE /user => deletes your own user
