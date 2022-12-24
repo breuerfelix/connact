f7898d8921de32375fd8fb736434ca4dfc255f7f
Tobias Brumhard <code@brumhard.com>
2022-12-24 01:41:09 +0100

    Add share page

---
+++ app/lib/screens/share.dart
@@ -0,0 +1,37 @@
+import 'package:app/services/users.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class SharePage extends StatelessWidget {
+  static String route = "/share";
+
+  const SharePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Provider.of<UsersService>(context, listen: false).currentUser,
+        builder: (context, snapshot) {
+          // TODO: copy error handling from other FutureBuilders
+          // and define as defaultFutureBuilder somewhere
+
+          if (snapshot.hasData) {
+            final username = snapshot.data!.username;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("@$username",
+                    style: Theme.of(context).textTheme.headline3),
+                QrImage(
+                  data: "https://connact.io/add/$username",
+                  size: 0.5 * MediaQuery.of(context).size.width,
+                )
+              ],
+            );
+          }
+
+          return const CircularProgressIndicator();
+        });
+  }
+}
