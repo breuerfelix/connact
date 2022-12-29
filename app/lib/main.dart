a391d9aeceba94b600cab5183f180b69bbc77d42
Tobias Brumhard <code@brumhard.com>
2022-12-29 22:33:57 +0100

    Custom bottom navigation bar design

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,7 +10,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
-import '../util/string_casing.dart';
+import 'util/string_casing.dart';
 
 void main() {
   runApp(const MyApp());
@@ -87,32 +87,66 @@ class MyApp extends StatelessWidget {
 // changing the AppBar and navigating with bottom navigation bar.
 // See https://github.com/flutter/flutter/issues/23106.
 // https://stackoverflow.com/questions/64618050/is-it-correct-to-have-nested-scaffold-in-flutter
+// The bottom app bar is trying to emulate this layout:
+// // TODO: try this design: https://www.uplabs.com/posts/bottom-navigation-bar-ui-kit-29531c96-5562-47c5-acec-3b52618e4af3.
   Widget _pageScaffoldBuilder(
       BuildContext context, GoRouterState state, Widget child) {
+    Map<String, IconData> pageToIcon = {
+      ContactsPage.route: FontAwesomeIcons.addressBook,
+      SharePage.route: FontAwesomeIcons.at,
+      ProfilePage.route: FontAwesomeIcons.person,
+    };
+    assert(pageToIcon.length.isOdd);
+
+    final middlePageIndex = (pageToIcon.length / 2).floor();
+    final middlePageKey = pageToIcon.keys.elementAt(middlePageIndex);
+    List<Widget> bottomBarItems = pageToIcon.entries
+        .map<Widget>((entry) => entry.key == middlePageKey
+            // replacing middle one to keep row spacing correct
+            // middle one is instead in the FAB
+            ? const SizedBox.shrink()
+            : _bottomButton(context, entry.key, entry.value))
+        .toList();
+
     return Scaffold(
       body: child,
-      bottomNavigationBar: _buildBottomNavigationBar(context),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: bottomBarItems,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.go(middlePageKey),
+        tooltip: middlePageKey,
+        child: Icon(pageToIcon[middlePageKey]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
 
-// TODO: try this design: https://www.uplabs.com/posts/bottom-navigation-bar-ui-kit-29531c96-5562-47c5-acec-3b52618e4af3
-// - share as FAB style in the middle
-// - order: settings, search, share, profile, contacts
-BottomNavigationBar _buildBottomNavigationBar(BuildContext context) {
-  final pageToIcon = {
-    ContactsPage.route: FontAwesomeIcons.addressBook,
-    SharePage.route: FontAwesomeIcons.share,
-    ProfilePage.route: FontAwesomeIcons.person,
-  };
-
-  return BottomNavigationBar(
-      currentIndex: pageToIcon.keys
-          .toList()
-          .indexWhere((element) => element == GoRouter.of(context).location),
-      onTap: (index) => context.go(pageToIcon.keys.elementAt(index)),
-      items: pageToIcon.entries
-          .map((p) => BottomNavigationBarItem(
-              icon: Icon(p.value), label: p.key.split('/').last.capitalize()))
-          .toList());
+Widget _bottomButton(BuildContext context, String page, IconData icon) {
+  return Tooltip(
+    message: page,
+    child: SizedBox.fromSize(
+      size: const Size(60, 60),
+      child: InkWell(
+        customBorder: const CircleBorder(),
+        onTap: () => context.go(page),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon), // <-- Icon
+            Text(
+              page.split('/').last.capitalize(),
+              style: Theme.of(context).textTheme.caption,
+            ), // <-- Text
+          ],
+        ),
+      ),
+    ),
+  );
 }
