99ed7e0e0cdcc5854161c91599e3bafb97be3811
Tobias Brumhard <code@brumhard.com>
2022-12-30 14:17:29 +0100

    Add logout button in settings

--- app/lib/screens/settings.dart
+++ app/lib/screens/settings.dart
@@ -1,4 +1,7 @@
+import 'package:app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   static String route = "/settings";
@@ -7,8 +10,22 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("settings")),
+    return Scaffold(
+      body: Center(
+        child: Ink(
+          decoration: ShapeDecoration(
+            shape: const CircleBorder(),
+            color: Theme.of(context).errorColor,
+          ),
+          child: IconButton(
+            iconSize: 50,
+            color: Colors.white,
+            onPressed: () =>
+                Provider.of<AuthService>(context, listen: false).logout(),
+            icon: const FaIcon(FontAwesomeIcons.powerOff),
+          ),
+        ),
+      ),
     );
   }
 }
