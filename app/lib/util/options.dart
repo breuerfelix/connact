5ee72aab3b3f2372fe7db201cbea59b9071c6e50
Tobias Brumhard <code@brumhard.com>
2023-01-03 13:41:55 +0100

    Refactor profile page, make fullname editable

--- app/lib/util/options.dart
+++ app/lib/util/options.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../util/string_validations.dart';
+
 // TODO: use consistent icons (outlines vs black etc.)
 
 class Options {
   static final map = {
     "phone": OptionData(icon: FontAwesomeIcons.phone),
     "facebook": OptionData(icon: FontAwesomeIcons.facebook),
-    "twitter":
-        OptionData(icon: FontAwesomeIcons.twitter, idFormatter: (id) => "@$id"),
+    "twitter": OptionData(icon: FontAwesomeIcons.twitter),
     "instagram": OptionData(icon: FontAwesomeIcons.instagram),
     "email": OptionData(
-        icon: FontAwesomeIcons.envelope,
-        formFieldValidator: (value) =>
-            value.isValidEmail ? null : "$value is not a valid email"),
+      icon: FontAwesomeIcons.envelope,
+      validator: (value) =>
+          value.isValidEmail ? null : "$value is not a valid email",
+    ),
   };
 }
 
 class OptionData {
-  late final FormFieldValidator<String> validator;
+  final FormFieldValidator<String>? validator;
   final IconData icon;
-  late final String Function(String id) identityFormatter;
 
-  OptionData({formFieldValidator, required this.icon, idFormatter}) {
-    validator = formFieldValidator ?? (value) => null;
-    identityFormatter = idFormatter ?? (id) => id;
-  }
+  OptionData({this.validator, required this.icon});
 }
