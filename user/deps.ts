69fc9213071017278dfc882024f547e212759c59
Felix Breuer <fbreuer@pm.me>
2023-04-12 22:52:26 +0200

    add new service and refactor user
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

---
+++ user/deps.ts
@@ -0,0 +1,8 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+import Surreal from "https://deno.land/x/surrealdb@v0.2.0/mod.ts";
+export { Surreal };
+
+export { verify } from "https://deno.land/x/djwt@v2.2/mod.ts";
