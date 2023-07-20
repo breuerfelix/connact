22f46164fdcbd37d616aee2b078c5163c5572e36
Felix Breuer <fbreuer@pm.me>
2023-07-20 21:30:21 +0200

    make namespace static
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- pulumi/namespace.ts
+++ pulumi/namespace.ts
@@ -1,8 +1,1 @@
-import * as k8s from "@pulumi/kubernetes"
-
-const name = "connact"
-const ns = new k8s.core.v1.Namespace(name, {
-    metadata: { name },
-})
-
-export const namespace = ns.metadata.name
+export const namespace = "connact"
